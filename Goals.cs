d513f383d66311428cd505f12b8ee55e89124f0b
mahaffay <mahaffayjustin@gmail.com>
2023-11-03 14:08:16 -0500

    Initial commit

---
+++ Goals.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoccerLeagueTable
+{
+    class Goals
+    {
+        public string team { get; set; }
+        public int goalsFor { get; set; }
+        public int goalsAgainst { get; set; }
+        public int difference { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+
+namespace SoccerLeagueTable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location),
+                @"football.dat");
+
+            List<Goals> goalList = new List<Goals>();
+            string[] files = File.ReadAllLines(path);
+
+            for (int m = 1; m < files.Length; m++)
+            {
+                string[] row = files[m].Replace("-","").Trim().Split(' ');
+                row = row.Where(x => !string.IsNullOrEmpty(x)).ToArray();
+
+                if (row.Length != 0)
+                {
+                    goalList.Add(new Goals()
+                    {
+                        team = row[1],
+                        goalsFor = Convert.ToInt16(row[6]),
+                        goalsAgainst = Convert.ToInt16(row[7]),
+                        difference = Convert.ToInt16(row[6]) - Convert.ToInt16(row[7])
+                    });
+                }
+            }
+
+            var result = goalList.OrderBy(a => a.difference);
+
+            var smallestDiff = result.Select(x => x.team).First();
+
+            Console.WriteLine(smallestDiff + " had the smallest difference in for and against goals");
+            Console.ReadKey();
+        }
+    }
+}
